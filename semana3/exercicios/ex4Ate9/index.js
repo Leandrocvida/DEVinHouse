caf5f3ef3dd90da3baa9573f7724ffe46705e128
leandrocvida <leandrocvida@gmail.com>
2022-07-15 17:05:37 -0300

    Ex4

---
+++ semana3/exercicios/ex4Ate9/index.js
@@ -0,0 +1,48 @@
+const pessoa1 = {
+    nome:"José",
+    sobreNome:"Das Coves",
+    idade: 21,
+    setor: "Camarote",
+};
+
+const pessoa2 = {
+    nome:"Maria",
+    sobreNome:"Silva",
+    idade: 17,
+    setor: "Camarote",
+};
+
+const pessoa3 = {
+    nome:"Marcelo",
+    sobreNome:"Macedo",
+    idade: 30,
+    setor: "Pista",
+};
+
+const pessoa4 = {
+    nome:"Julia",
+    sobreNome:"Cravo",
+    idade: 25,
+    setor: "Pista",
+};
+
+const pessoa5 = {
+    nome:"Marilia",
+    sobreNome:"Mendonça",
+    idade: 41,
+    setor: "Arquibancada",
+};
+
+const pessoa6 = {
+    nome:"Dejair",
+    sobreNome:"Das Neves",
+    idade: 26,
+    setor: "Arquibancada",
+};
+
+const pessoa7 = {
+    nome:"Amanda",
+    sobreNome:"Oliveira",
+    idade: 16,
+    setor: "Arquibancada",
+};
