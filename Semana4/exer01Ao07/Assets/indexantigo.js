0f87cea726a5a751f6a2fbf27f2b64b0cb57319c
leandrocvida <leandrocvida@gmail.com>
2022-07-24 20:36:36 -0300

    ex4

---
+++ Semana4/exer01Ao07/Assets/indexantigo.js
@@ -0,0 +1,66 @@
+let contasClientes = [
+    {
+      id: 1,
+      nome: 'Cliente 01',
+      saldo: 500,
+    },
+    {
+      id: 2,
+      nome: 'Cliente 02',
+      saldo: 3000,
+    },
+    {
+      id: 3,
+      nome: 'Cliente 03',
+      saldo: 5000,
+    },
+  ];
+  
+var selectContas = document.getElementById("selectContas");
+
+ var inserirOpcSelect = contasClientes.forEach( (cliente) => {
+    let clienteInserir = `<option id="idDoCliente${cliente.id}" value="${cliente.nome}">${cliente.nome}</option>`;
+    selectContas.insertAdjacentHTML('afterbegin',clienteInserir);
+    });
+
+    // exercicio 4
+    
+    
+function sacarDinheiro() {
+   
+    let qualCliente = selectContas.options[selectContas.selectedIndex].value;  // puxa qual dos clientes a partir do select
+    let indiceDadosDoCliente = contasClientes.findIndex(elemento => elemento.nome === qualCliente); //acha o indice
+    let dadosDoCliente;
+    if (indiceDadosDoCliente!== -1) {
+        dadosDoCliente = contasClientes[indiceDadosDoCliente];
+    } //cria uma variavel com referencia ao objeto original pra puxar o "cliente" direto array permitindo modificação do array orginal
+    var valorEmConta = dadosDoCliente.saldo;
+    var dinheiroASacar = parseInt(document.getElementById("valor").value); //puxa o valor digitado no imput do HTML
+    if (dinheiroASacar <= 0) {
+        alert("Valor Inválido");
+        
+    } else if ( dinheiroASacar > valorEmConta ){
+        alert(`Saldo insuficiente! Você possui ${valorEmConta} reais em sua conta`);
+        
+    } else if ( dinheiroASacar <= valorEmConta ){
+        valorEmConta = valorEmConta - dinheiroASacar;
+        dadosDoCliente.saldo = valorEmConta;
+        alert(`Saque bem sucedido! Você ainda possui ${valorEmConta} reais em sua conta.`);    
+        return;
+    } 
+};
+
+
+
+
+var operacao = document.getElementById("operacao");
+
+function aoclique() {
+    event.preventDefault()
+    let qualOperacao = operacao.options[operacao.selectedIndex].value;
+    if (qualOperacao === "sacar") {
+        sacarDinheiro()
+    } else if (qualOperacao === "depositar")  {
+        alert("deposito")
+    }  
+};
