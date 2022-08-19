49b0688e16e396e10bc209610e479e1b9f0ade57
leandrocvida <leandrocvida@gmail.com>
2022-08-19 00:43:59 -0300

    ex06

--- Semana8/Exercicios/ArquivoBase/src/components/ListaCards/ListaCards.jsx
+++ Semana8/Exercicios/ArquivoBase/src/components/ListaCards/ListaCards.jsx
@@ -6,14 +6,15 @@ import styles from './ListaCards.module.css';
 import { useState } from 'react';
 
 export const ListaCards = ({ produtos }) => {
-  
+
   const[listaIdSelecionados,setlistaIdSelecionados] = useState([])
 
+  const handleSelecionar = () => {} 
   return(
     <ul className={styles.lista}>
       {produtos.map((produto) => (
         <li className={styles.listaItem} key={produto.id}>
-          <Card produto={produto} />
+          <Card produto={produto} onSelecionar ={()=> handleSelecionar(produto.id)} selecionado = { listaIdSelecionados.includes(produto.id)}/>
         </li>
       ))}
     </ul>
