e7113ac871ab254367173c62d58adb7ce63cd9e0
leandrocvida <leandrocvida@gmail.com>
2022-08-28 21:41:49 -0300

    e10

--- Semana9/exercicios/src/contexts/prodSelect/ProdSelecProvider.jsx
+++ Semana9/exercicios/src/contexts/prodSelect/ProdSelecProvider.jsx
@@ -1,33 +1,39 @@
-import {useState} from "react"
-import PropTypes from 'prop-types';
-import { ProdSelecContext } from "."
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { ProdSelecContext } from ".";
 
+export const ProdSelecProvider = ({ children }) => {
+  const [produtosSelecionados, setProdutosSelecionados] = useState([]);
 
-        
-export const ProdSelecProvider = ({children}) => {
-    const [produtosSelecionados,setProdutosSelecionados]= useState([]);
+  const isSelected = (id) => {
+    return produtosSelecionados.some((item) => item.id === id);
+  };
+
+  const handleSelecionar = (prod) => {
+    if (isSelected(prod.id)) {
+      setProdutosSelecionados(
+        produtosSelecionados.filter((item) => item.id !== prod.id)
+      );
+      return;
+    }
+
+    setProdutosSelecionados([...produtosSelecionados, prod]);
+  };
+
+  const limpaSelec = () => {
     
-    const isSelected = (id) => {
-        return produtosSelecionados.some((item) => item.id === id);
-      };
+    setProdutosSelecionados([]);
     
-      const handleSelecionar = (prod) => {
-        if (isSelected(prod.id)) {
-          setProdutosSelecionados(produtosSelecionados.filter((item) => item.id !== prod.id));
-          return;
-        }
-    
-        setProdutosSelecionados([...produtosSelecionados, prod]);
-      };
+  };
 
-    return( 
-
-    <ProdSelecContext.Provider value = {{produtosSelecionados, handleSelecionar, isSelected}}>
-        {children}
+  return (
+    <ProdSelecContext.Provider
+      value={{ produtosSelecionados, handleSelecionar, isSelected, limpaSelec }}
+    >
+      {children}
     </ProdSelecContext.Provider>
-)
-
-}
+  );
+};
 ProdSelecProvider.propTypes = {
-    children: PropTypes.node,
-}
+  children: PropTypes.node,
+};
--- Semana9/exercicios/src/pages/produtosSelecionados/ProdutosSelecionados.jsx
+++ Semana9/exercicios/src/pages/produtosSelecionados/ProdutosSelecionados.jsx
@@ -1,26 +1,24 @@
-import { ListaCards } from "@components"
-import { useProdSelec } from "@contexts"
+import { ListaCards } from "@components";
+import { useProdSelec } from "@contexts";
 import { formataValor } from "@utils";
 import styles from "./ProdutosSelecionados.module.css";
 
 export const ProdutosSelecionados = () => {
-   const {produtosSelecionados} = useProdSelec()
-   const total = produtosSelecionados.reduce((soma, item) =>{
+  const { produtosSelecionados, limpaSelec } = useProdSelec();
+  const total = produtosSelecionados.reduce((soma, item) => {
     return soma + item.valor;
-   }, 0);
-   const totalFormatado = formataValor(total)
-   
-   
-   return(
-        <>
-        <h1>Produtos Selecionados</h1>
-        <ListaCards produtos = {produtosSelecionados}/>
-        <div className = {styles.div}>
-            <h2 className = {styles.h2}> Valor total:</h2>
-            <h2 className = {styles.h2}>{totalFormatado}</h2>
-        </div>
-        </>
+  }, 0);
+  const totalFormatado = formataValor(total);
 
-)
-
-}
+  return (
+    <>
+      <h1>Produtos Selecionados</h1>
+      <ListaCards produtos={produtosSelecionados} />
+      <div className={styles.div}>
+        <h2 className={styles.h2}> Valor total:</h2>
+        <h2 className={styles.h2}>{totalFormatado}</h2>
+        <button onClick={limpaSelec} >Limpar Seleção</button>
+      </div>
+    </>
+  );
+};
